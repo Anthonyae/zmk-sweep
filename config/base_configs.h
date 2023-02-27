5c9fc53b109b030a59970920195af00cbf9357b9
Anthony Alvarez <anthony.ae@outlook.com>
2023-02-27 00:38:25 -0800

    fixed the behaviors

--- config/base_configs.h
+++ config/base_configs.h
@@ -51,8 +51,8 @@
 #define hms_base 1000
 #define HMS_VVSLO (hms_base + 300)
 #define HMS_VSLO (hms_base + 100)
-// #define HMS_SLOW (hms_base + 50)
-// #define HMS_EXP  (hms_base + 20)
-// #define HMS_MEDI (hms_base - 50)
+#define HMS_SLOW (hms_base + 50)
+#define HMS_EXP  (hms_base + 20)
+#define HMS_MEDI (hms_base - 50)
 #define HMS_FAST (hms_base - 150)
 #define HMS_VFAS 130
