0ec5924c119370e22d53f8d9250d0ebec3fe5de0
anthony.ae <anthony.ae@outlook.com>
2023-04-30 00:19:37 -0700

    added the right labeling for homerow mod changes

--- config/base_configs.h
+++ config/base_configs.h
@@ -1,19 +1,20 @@
 // Layer definitions
-#define DEFAULT 0
-#define DEFAULT_NO_HM 1
-#define DEFAULT_LONG_HM 2
-#define SYMBOL 3
-#define NUMBER 4
-#define NAV 5
-#define UTILITY 6
-#define MOUSE_BASE 7
-#define FUNC 8
-#define MOUSE_FAST 9
-#define MOUSE_MEDIUM 10
-#define MOUSE_SLOW 11
-#define MOUSE_CRAWL 12
-#define NUMBER_TEST 13
-#define CONVENIENCE_LAYER 14
+#define DEFAULT_HM 0
+#define DEFAULT_HM_ONE 1
+#define DEFAULT_HM_TWO 2
+#define DEFAULT_HM_NONE 3
+#define SYMBOL 4
+#define NUMBER 5
+#define NAV 6
+#define UTILITY 7
+#define MOUSE_BASE 8
+#define FUNC 9
+#define MOUSE_FAST 10
+#define MOUSE_MEDIUM 11
+#define MOUSE_SLOW 12
+#define MOUSE_CRAWL 13
+#define NUMBER_TEST 14
+#define CONVENIENCE_LAYER 15
 
 // global settings
 &mt {
