c5f543b53172c539514d2bbb6560d6dfe09635f9
anthony.ae <anthony.ae@outlook.com>
2023-08-06 17:08:23 -0500

    added another layer

--- config/base_configs.h
+++ config/base_configs.h
@@ -2,19 +2,20 @@
 #define DEFAULT_HM 0
 #define DEFAULT_HM_ONE 1
 #define DEFAULT_HM_TWO 2
-#define DEFAULT_HM_NONE 3
-#define SYMBOL 4
-#define NUMBER 5
-#define NAV 6
-#define UTILITY 7
-#define MOUSE_BASE 8
-#define FUNC 9
-#define MOUSE_FAST 10
-#define MOUSE_MEDIUM 11
-#define MOUSE_SLOW 12
-#define MOUSE_CRAWL 13
-#define MOUSE_DEAD 14
-#define CONVENIENCE_LAYER 15
+#define DEFAULT_HM_THREE 3
+#define DEFAULT_HM_NONE 4
+#define SYMBOL 5
+#define NUMBER 6
+#define NAV 7
+#define UTILITY 8
+#define MOUSE_BASE 9
+#define FUNC 10
+#define MOUSE_FAST 11
+#define MOUSE_MEDIUM 12
+#define MOUSE_SLOW 13
+#define MOUSE_CRAWL 14
+#define MOUSE_DEAD 15
+#define CONVENIENCE_LAYER 16
 
 
 // global settings
