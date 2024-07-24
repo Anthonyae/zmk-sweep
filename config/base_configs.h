c4e11c3118f078f02b06009902c2a54e7e825a76
Tony <anthony.ae@outlook.com>
2024-07-24 18:17:43 -0500

    chore: testing: removing variables and layers

--- config/base_configs.h
+++ config/base_configs.h
@@ -18,32 +18,33 @@
 #define CONVENIENCE_LAYER 16
 #define LAYER_CHANGER 17
 
+
 // global settings
-&mt
-{
-  tapping - term - ms = <200>;
+&mt {
+    tapping-term-ms = <200>;
 };
 
-&lt
-{
-  tapping - term - ms = <130>;
+&lt {
+    tapping-term-ms = <130>; 
 };
 
 // this changes the movement speed of the mouse
 // time-to-max-speed-ms = <400>; // trying to speed up the mouse movement
 // time-to-max-speed-ms = <385>;
-&mmv
-{
-  time - to - max - speed - ms = <360>;
-  acceleration - exponent = <1>;
+&mmv {
+  time-to-max-speed-ms = <360>;
+  acceleration-exponent = <1>;
 };
 
 // this changes the scrolling behavior
+&mwh {
+  time-to-max-speed-ms = <300>;
+  acceleration-exponent = <0>;
+};
 
-&sk
-{
-  release - after - ms = <400>;
-  quick - release;
+&sk {
+    release-after-ms = <400>;
+    quick-release;
 };
 
 // global keys
