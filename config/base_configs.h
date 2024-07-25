90850e51f98d469b0b3b5d3ed78c7a8cf21e7189
Tony <anthony.ae@outlook.com>
2024-07-24 20:17:21 -0500

    chore: moved all base_configs to both keymap and it removed some

--- config/base_configs.h
+++ config/base_configs.h
@@ -35,11 +35,16 @@
   acceleration-exponent = <1>;
 };
 
-// this changes the scrolling behavior
-&amwh {
-  time-to-max-speed-ms = <300>;
-  acceleration-exponent = <0>;
+&sk {
+    release-after-ms = <400>;
+    quick-release;
 };
+
+// this changes the scrolling behavior
+// &mwh {
+//   time-to-max-speed-ms = <300>;
+//   acceleration-exponent = <0>;
+// };
 
 // global keys
 #define HYPER LG(LS(LA(LCTRL)))
